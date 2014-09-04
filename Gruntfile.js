be14748d9c2d6fcbbf2b3396bd6b9190778fbcbc
Skeiter9 <Skeiter97@gmail.com>
2014-09-04 17:35:56 -0500

    some com

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt){
+	// config
+	grunt.initConfig({
+		watch:{ 
+			scripts:{
+        files:['./*.html','_components/*.html','js/*.js','js/directives/*.js','js/services/*.js','css/*.css'],
+        options: {
+         	livereload: 1337
+        }
+      },
+			sass:{
+				files:['scss/*.scss', 'scss/components/*.scss'],
+				tasks:['sass']
+			}
+		},
+		connect: {
+		  server: {
+		    options: {
+		    	port: 9002,
+		    	base:'.',
+		     	livereload: 1337
+		    }
+		  }
+		},
+	  sass: {
+	      dist: {
+	        files: {
+	          'css/style.css':'scss/style.scss'
+	        }
+	      },
+        options:{
+          lineNumbers: true
+        }
+	  }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default',['connect','watch'] );
+	grunt.registerTask('compile-sass',[ 'sass'] );
+
+};
